f395f1134aa4df7f1e92ee2e7564b6b8c0775808
Sam <144938568+SamLo2200@users.noreply.github.com>
2024-07-16 06:43:04 +0800

    Initial Commit

---
+++ src/main/java/com/itdognorights/screwingUpMinecraft/ScrewingUpMinecraft.java
@@ -0,0 +1,17 @@
+package com.itdognorights.screwingUpMinecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ScrewingUpMinecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
